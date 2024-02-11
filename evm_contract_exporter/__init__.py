c86a3852fb39240a629c9b0069a0c5235719ea10
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-02-11 00:41:27 +0000

    feat: import ContractCallMetric to main module

--- evm_contract_exporter/__init__.py
+++ evm_contract_exporter/__init__.py
@@ -3,6 +3,7 @@ from generic_exporters import Constant, Metric, TimeSeries, WideTimeSeries
 
 from evm_contract_exporter.contract import GenericContractExporter, ContractExporterBase
 from evm_contract_exporter.exporters import ContractMetricExporter, ViewMethodExporter
+from evm_contract_exporter.metric import ContractCallMetric
 from evm_contract_exporter.scale import Scale, SmartScale
 
 __all__ = [
@@ -11,6 +12,7 @@ __all__ = [
     ContractMetricExporter,
     ViewMethodExporter,
     Metric,
+    ContractCallMetric,
     Constant,
     Scale,
     SmartScale,
