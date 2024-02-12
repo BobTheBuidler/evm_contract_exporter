5dcc85555d272c3d2521340b947ac0f14a9881d8
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-02-12 03:53:53 +0000

    chore: refactor

--- evm_contract_exporter/utils.py
+++ evm_contract_exporter/utils.py
@@ -13,6 +13,7 @@ from evm_contract_exporter import types
 if TYPE_CHECKING:
     from evm_contract_exporter.exporters.method import Scaley
 
+
 _block_timestamp_semaphore = a_sync.PrioritySemaphore(500, name="block for timestamp semaphore")
 
 async def get_block_at_timestamp(timestamp: datetime) -> int:
@@ -20,11 +21,9 @@ async def get_block_at_timestamp(timestamp: datetime) -> int:
     async with _block_timestamp_semaphore[0 - timestamp.timestamp()]:  # NOTE: We invert the priority to go high-to-low so we can get more recent data more quickly
         return await y.get_block_at_timestamp(timestamp)
 
-_deploy_block_semaphore = a_sync.Semaphore(100, "deploy block semaphore")
-
+@a_sync.Semaphore(100, "deploy block semaphore")
 async def get_deploy_block(contract: types.address) -> int:
-    async with _deploy_block_semaphore:
-        return await y.contract_creation_block_async(contract)
+    return await y.contract_creation_block_async(contract)
 
 def wrap_contract(contract: y.Contract, scale: "Scaley" = True) -> y.Contract:
     """Converts all `ContractCall` objects in `contract.__dict__` to `ContractCallMetric` objects with more functionality"""
