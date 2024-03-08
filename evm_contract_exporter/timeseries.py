ab47c80b537f462fe901cb54c32731640987f750
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-03-08 09:44:14 +0000

    fix: broken import

--- evm_contract_exporter/timeseries.py
+++ evm_contract_exporter/timeseries.py
@@ -4,14 +4,13 @@ from typing import Union, final
 import generic_exporters
 
 from evm_contract_exporter import types
-from evm_contract_exporter._address import _AddressKeyedMetric
-from evm_contract_exporter.metric import AnyContractCallMetric
+from evm_contract_exporter.metric import _MetricBase, AnyContractCallMetric
 
 
 class TimeSeries(generic_exporters.TimeSeries):  # type: ignore [misc]
     """This helper class is a `TimeSeries` object for a `Metric` that relates to a specific wallet address"""
-    metric: _AddressKeyedMetric
-    def __init__(self, metric: _AddressKeyedMetric, sync: bool = True) -> None:
+    metric: _MetricBase
+    def __init__(self, metric: _MetricBase, sync: bool = True) -> None:
         super().__init__(metric, sync=sync)
     @property
     def address(self) -> types.address:
@@ -40,9 +39,12 @@ class ContractCallTimeSeries(TimeSeries):  # type: ignore [misc]
         metric_repr = repr(self.metric)
         return f"<{self.__class__.__name__} {metric_repr[metric_repr.find('0x'):]}"
     
-SingleAddressKeyedProcessable = Union[_AddressKeyedMetric, TimeSeries]
+SingleProcessable = Union[_MetricBase, TimeSeries]
 
 @final
 class WideTimeSeries(generic_exporters.WideTimeSeries):  # type: ignore [misc]
-    def __init__(self, *timeserieses: SingleAddressKeyedProcessable, sync: bool = True) -> None:
-        super().__init__(*(TimeSeries(x) for x in timeserieses if isinstance(x, _AddressKeyedMetric)), sync=sync)
+    def __init__(self, *timeserieses: SingleProcessable, sync: bool = True) -> None:
+        for x in timeserieses:
+            if not isinstance(x, (TimeSeries, _MetricBase)):
+                raise TypeError(f"`x` must be a `TimeSeries` or a `Metric` object. You passed {x}")
+        super().__init__(*(TimeSeries(x) for x in timeserieses if isinstance(x, _MetricBase)), sync=sync)
