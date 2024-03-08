448b637ccbe3cc3d9c52fa53b3cc8f7b59a4fde3
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-03-08 09:46:49 +0000

    chore: refactor

--- evm_contract_exporter/timeseries.py
+++ evm_contract_exporter/timeseries.py
@@ -8,7 +8,6 @@ from evm_contract_exporter.metric import _MetricBase, AnyContractCallMetric
 
 
 class TimeSeries(generic_exporters.TimeSeries):  # type: ignore [misc]
-    """This helper class is a `TimeSeries` object for a `Metric` that relates to a specific wallet address"""
     metric: _MetricBase
     def __init__(self, metric: _MetricBase, sync: bool = True) -> None:
         super().__init__(metric, sync=sync)
@@ -19,22 +18,6 @@ class TimeSeries(generic_exporters.TimeSeries):  # type: ignore [misc]
 class ContractCallTimeSeries(TimeSeries):  # type: ignore [misc]
     # TODO maybe just combine this into `TimeSeries`
     metric: "AnyContractCallMetric"
-    def __init__(
-        self, 
-        metric: "AnyContractCallMetric", 
-        *, 
-        #scale: Union[bool, int, Scale] = False, 
-        sync: bool = True,
-    ) -> None:
-        super().__init__(metric, sync)
-        '''
-        self._scale = scale
-        try:
-            if self._scale and not self.metric._can_scale:
-                self._scale = False
-        except AttributeError:
-            self._scale = False
-        '''
     def __repr__(self) -> str:
         metric_repr = repr(self.metric)
         return f"<{self.__class__.__name__} {metric_repr[metric_repr.find('0x'):]}"
