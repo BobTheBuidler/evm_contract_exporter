6854619ce8d28115f6eb8f1a8ccd89bf73d11479
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-03-14 07:37:01 +0000

    chore: refactor ensure_data

--- evm_contract_exporter/exporters/_base.py
+++ evm_contract_exporter/exporters/_base.py
@@ -38,23 +38,24 @@ class _ContractMetricExporterBase(_ContractMetricProcessorBase, TimeSeriesExport
         return await asyncio.gather(*[self.datastore.data_exists(field.address, field.key, ts) for field in self.query.metrics])
 
     async def ensure_data(self, ts: datetime) -> None:
-        if semaphore := self._semaphore:
-            async with semaphore[0-ts.timestamp()]:
-                await self._ensure_data(ts)
-        else:
-            await self._ensure_data(ts)
-    
-    async def _ensure_data(self, ts: datetime) -> None:
         exists = await self.data_exists(ts, sync=False)
         if all(exists):
             logger.debug('complete data for %s at %s already exists in datastore', self, ts)
             return
-        elif any(exists):
-            coros: Dict[Metric, Coroutine[Any, Any, Decimal]] = {field: field.produce(ts, sync=False) for field, field_exists in zip(self.query.metrics, exists) if not field_exists}
+        if semaphore := self._semaphore:
+            async with semaphore[0-ts.timestamp()]:
+                await self._ensure_data(ts, exists)
+        else:
+            await self._ensure_data(ts, exists)
+    
+    async def _ensure_data(self, ts: datetime, data_exists: List[bool]) -> None:
+        if any(data_exists):
+            coros: Dict[Metric, Coroutine[Any, Any, Decimal]] = {field: field.produce(ts, sync=False) for field, field_exists in zip(self.query.metrics, data_exists) if not field_exists}
             data: AsyncIterable[Tuple[Metric, Union[Decimal, Exception]]] = a_sync.as_completed(coros, return_exceptions=True, aiter=True)
         else:
             logger.debug('no data exists for %s, exporting...', self)
             tasks = self.query[ts].tasks
+            # make sure the tasks are all created
             await tasks._init_loader
             data = a_sync.as_completed(tasks, return_exceptions=True, aiter=True)
         insert_tasks = {}
