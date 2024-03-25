402f1540df91eb58cd5d2db6d2d969f68a83fedc
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-03-25 04:25:09 +0000

    fix: leaky deploy block helper

--- evm_contract_exporter/processors/_base.py
+++ evm_contract_exporter/processors/_base.py
@@ -10,10 +10,10 @@ import a_sync
 import eth_retry
 from generic_exporters import QueryPlan
 from generic_exporters.processors._base import _TimeSeriesProcessorBase
-from y import get_block_at_timestamp
+from y import contract_creation_block_async, get_block_at_timestamp
 from y.time import get_block_timestamp_async
 
-from evm_contract_exporter import utils
+from evm_contract_exporter import types, utils
 from evm_contract_exporter.metric import Metric
 
 
@@ -46,7 +46,7 @@ class _ContractMetricProcessorBase(_TimeSeriesProcessorBase):
         return None
     @eth_retry.auto_retry
     async def start_timestamp(self) -> datetime:
-        earliest_deploy_block = min(await asyncio.gather(*[utils.get_deploy_block(field.address) for field in self.query.metrics]))
+        earliest_deploy_block = await self._earliest_deploy_block()
         deploy_timestamp = datetime.fromtimestamp(await get_block_timestamp_async(earliest_deploy_block), tz=timezone.utc)
         iseconds = self.query.interval.total_seconds()
         rounded_down = datetime.fromtimestamp(deploy_timestamp.timestamp() // iseconds * iseconds, tz=timezone.utc)
@@ -68,3 +68,13 @@ class _ContractMetricProcessorBase(_TimeSeriesProcessorBase):
         retval = await self.query[timestamp]
         logger.debug("%s produced %s at %s block %s", self, retval, timestamp, block)
         return retval
+    async def _earliest_deploy_block(self) -> int:
+        await self._load_deploy_blocks_to_memory()
+        return min(await asyncio.gather(*[contract_creation_block_async(field.address) for field in self.query.metrics]))
+    async def _load_deploy_blocks_to_memory(self) -> None:
+        """ensure the deploy block for all relevant contracts is cached in memory before proceeding"""
+        await utils.start_deploy_block_workers()
+        contract: types.address
+        for contract in {field.address for field in self.query.metrics}:
+            utils._deploy_block_queue.put_nowait(contract)
+        await utils._deploy_block_queue.join()
