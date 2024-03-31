51b771f032a3d14d18c18103c32079e1a3685c80
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-03-30 23:50:10 -0400

    feat(docs): set docs options

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,21 @@ intersphinx_mapping = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+autodoc_default_options = {
+    'private-members': True,
+    'special-members': '__init__,__call__,__getitem__,__iter__,__aiter__,__next__,__anext__',
+    'inherited-members': True,
+    'member-order': 'groupwise',
+    # hide private methods that aren't relevant to us here
+    #'exclude-members': '_abc_impl,_fget,_fset,_fdel,_ASyncSingletonMeta__instances,_ASyncSingletonMeta__lock'
+}
+autodoc_typehints = "description"
+# Don't show class signature with the class' name.
+autodoc_class_signature = "separated"
+
+automodule_generate_module_stub = True
 
 sys.path.insert(0, os.path.abspath('./evm_contract_exporter'))
